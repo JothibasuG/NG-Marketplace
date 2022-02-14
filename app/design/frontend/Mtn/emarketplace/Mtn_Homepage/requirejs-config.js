4acee3184064651234fea833f23240fd0f6075da
Faizan Ullah Beg <FB00783411@techmahindra.com>
2022-02-14 12:55:54 +0530

    home page changes

--- app/design/frontend/Mtn/emarketplace/Mtn_Homepage/requirejs-config.js
+++ app/design/frontend/Mtn/emarketplace/Mtn_Homepage/requirejs-config.js
@@ -3,11 +3,11 @@ var config = {
   // When load 'requirejs' always load the following files also
   deps: [
 //    "Magento_Theme/js/bootstrap.min",
-    "Magento_Theme/js/jquery.jcarousel.min",
-    "Magento_Theme/js/jquery.jcarousel-swipe",
-    "Magento_Theme/js/jcarousel.responsive",
-    "Magento_Theme/js/tab",
-    "Magento_Theme/js/custom"
+    "Mtn_Homepage/js/jquery.jcarousel.min",
+    "Mtn_Homepage/js/jquery.jcarousel-swipe",
+    "Mtn_Homepage/js/jcarousel.responsive",
+    "Mtn_Homepage/js/tab",
+    "Mtn_Homepage/js/custom"
   ]
 
 };
