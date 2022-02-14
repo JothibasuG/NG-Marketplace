f3ac14eec2267e6ba726c0f8848f453cb5cdbbeb
Faizan Ullah Beg <FB00783411@techmahindra.com>
2022-02-14 12:39:36 +0530

    home page changes

---
+++ app/design/frontend/Mtn/emarketplace/Mtn_Homepage/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+
+  // When load 'requirejs' always load the following files also
+  deps: [
+//    "Magento_Theme/js/bootstrap.min",
+    "Magento_Theme/js/jquery.jcarousel.min",
+    "Magento_Theme/js/jquery.jcarousel-swipe",
+    "Magento_Theme/js/jcarousel.responsive",
+    "Magento_Theme/js/tab",
+    "Magento_Theme/js/custom"
+  ]
+
+};
